cec5682077a95d58e8676ce2e38665228f16cff8
Ian Chen <ianchensoftware@gmail.com>
2026-02-22 20:01:59 -0600

    draft4: early stop

--- scripts/fb6fbaba/local/kcore.py
+++ scripts/fb6fbaba/local/kcore.py
@@ -4,8 +4,8 @@ from pathlib import Path
 import networkx as nx
 
 
-def get_comm(g: nx.Graph, q: int, k: int) -> set[int]:
-    def update_lowers(subg: nx.Graph, lowers):
+def get_comm(g: nx.Graph, q: int, query_k: int) -> set[int]:
+    def update_lowers(subg: nx.Graph, lowers, finished):
         # subg = nx.induced_subgraph(subg, set(subg.nodes).difference(frntr))
         degrees = dict(subg.degree)
 
@@ -20,7 +20,10 @@ def get_comm(g: nx.Graph, q: int, k: int) -> set[int]:
                 v = queues[k].pop()
                 if lowers[v] != k:
                     lowers[v] = k
-                    update = True
+
+                    update = v not in finished
+                    if k >= query_k:
+                        finished.add(v)
 
                 for u in subg.neighbors(v):
                     if degrees[u] > k:
@@ -29,7 +32,7 @@ def get_comm(g: nx.Graph, q: int, k: int) -> set[int]:
                         degrees[u] -= 1
         return update
 
-    def update_uppers(subg: nx.Graph, uppers):
+    def update_uppers(subg: nx.Graph, uppers, finished):
         def local_core(v):
             nums = [0] * (uppers[v] + 1)
             for u in g.neighbors(v):
@@ -46,11 +49,11 @@ def get_comm(g: nx.Graph, q: int, k: int) -> set[int]:
             c_new = local_core(v)
             if c_new != uppers[v]:
                 uppers[v] = c_new
-                update = True
+                update = v not in finished
 
         return update
 
-    def update_subgraph(g: nx.Graph, subg: nx.Graph, lowers, uppers, q, k):
+    def update_subgraph(g: nx.Graph, subg: nx.Graph, lowers, uppers):
         num_nodes = subg.number_of_nodes()
         update = False
         visited = {q}
@@ -67,7 +70,7 @@ def get_comm(g: nx.Graph, q: int, k: int) -> set[int]:
                     lowers[u] = 0
                     uppers[u] = g.degree(u)
 
-                if uppers[u] >= max(lowers[q], k):
+                if uppers[u] >= max(lowers[q], query_k):
                     visited.add(u)
 
                     if u not in subg.nodes:
@@ -82,6 +85,7 @@ def get_comm(g: nx.Graph, q: int, k: int) -> set[int]:
         return new_subg, update
 
     lowers, uppers = dict(), dict()
+    finished = set()
 
     # initialize with the 1-hop neighborhood of q
     subg = nx.induced_subgraph(g, list(g.neighbors(q)) + [q])
@@ -99,13 +103,13 @@ def get_comm(g: nx.Graph, q: int, k: int) -> set[int]:
     all_visited = set(subg.nodes).copy()
 
     update = True
-    while update and uppers[q] >= k:
-        # print(f"{lowers[q]=}, {uppers[q]=} {subg.number_of_nodes()=}")
+    while update and uppers[q] >= query_k:
+        print(f"{lowers[q]=}, {uppers[q]=} {subg.number_of_nodes()=}")
 
-        update_lower = update_lowers(subg, lowers)
-        update_upper = update_uppers(subg, uppers)
+        update_lower = update_lowers(subg, lowers, finished)
+        update_upper = update_uppers(subg, uppers, finished)
 
-        subg, update_subg = update_subgraph(g, subg, lowers, uppers, q, k)
+        subg, update_subg = update_subgraph(g, subg, lowers, uppers)
         update = update_lower | update_upper | update_subg
 
         all_visited = all_visited.union(set(subg.nodes))
