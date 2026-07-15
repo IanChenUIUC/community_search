353867c04a307615135fe9d8862f281ef5011f2c
Ian <ianchen3@illinois.edu>
2026-07-14 22:15:11 -0500

    give network-stats more memory; flushing

--- src/networkit-core-decomposition/network-stats.py
+++ src/networkit-core-decomposition/network-stats.py
@@ -8,20 +8,21 @@ import networkit as nk
 @click.option("--edgelist", required=True, type=click.Path(exists=True))
 def stats(edgelist):
     graph = nk.graphio.EdgeListReader(",", 0, "s", continuous=True).read(edgelist)
-    print(f"n = {graph.numberOfNodes()}, m = {graph.numberOfEdges()}")
+    print(f"n = {graph.numberOfNodes()}, m = {graph.numberOfEdges()}", flush=True)
 
     lcc = nk.centrality.LocalClusteringCoefficient(graph)
     scores = np.mean(lcc.run().scores())
-    print("average clustering coeff =", float(scores))
+    print("average clustering coeff =", float(scores), flush=True)
     del lcc
 
     cc = nk.components.ConnectedComponents(graph).run()
     giant = cc.extractLargestConnectedComponent(graph, compactGraph=True)
-    print(f"giant component: n = {giant.numberOfNodes()} m = {giant.numberOfEdges()}")
+    giant_n, giant_m = giant.numberOfNodes(), giant.numberOfEdges()
+    print(f"giant component: n = {giant_n} m = {giant_m}", flush=True)
     del cc
 
     ed = nk.distance.EffectiveDiameter(giant, ratio=0.9)
-    print("effective diameter =", f"{ed.run().getEffectiveDiameter():.3f}")
+    print("effective diameter =", f"{ed.run().getEffectiveDiameter():.3f}", flush=True)
     del ed
 
 
